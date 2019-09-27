252235ed33effedd8cda7177979993942e440735
CarlosUPC <wolfsea.yt@gmail.com>
2019-09-27 18:06:34 +0200

    added perfTimer + typedef type data

--- Chrono Stasis Engine/Engine/src/Core/csGlobals.h
+++ Chrono Stasis Engine/Engine/src/Core/csGlobals.h
@@ -19,6 +19,9 @@ void log(const char file[], int line, const char* format, ...);
 
 
 typedef unsigned int uint;
+typedef unsigned __int32 uint32;
+typedef unsigned __int64 uint64;
+typedef unsigned char uchar;
 
 enum update_status
 {
