07887abfcff2981749e36c70ee09efedb90cd3d6
Sebi-Lopez <sebi8818@gmail.com>
2019-09-29 18:44:45 +0200

    About Window versions linked

--- Chrono Stasis Engine/Engine/src/Structure/AboutWindow.cpp
+++ Chrono Stasis Engine/Engine/src/Structure/AboutWindow.cpp
@@ -50,19 +50,22 @@ void AboutWindow::MadeBy()
 		App->SendToLink("https://github.com/Sebi-Lopez");
 
 	ImGui::NewLine(); 
+	ImGui::NewLine(); 
 }
 
 void AboutWindow::SoftwareUsed()
 {
 	ImGui::Text("3rd Parties Libraries Used: ");
 
-	ImGui::Text("  SDL - Simple Direct Media v2.0"); 
+	SDL_version compiled;
+	SDL_VERSION(&compiled);
+	ImGui::Text("  SDL - Simple Direct Media %d.%d.%d", compiled.major, compiled.minor, compiled.patch);
 	if (ImGui::IsItemHovered())
 		ImGui::SetMouseCursor(ImGuiMouseCursor_::ImGuiMouseCursor_Hand);
 	if (ImGui::IsItemClicked())
 		App->SendToLink("https://www.libsdl.org");
 
-	ImGui::Text("  ImGui v1.73");
+	ImGui::Text("  ImGui v%s", ImGui::GetVersion());	
 	if (ImGui::IsItemHovered())
 		ImGui::SetMouseCursor(ImGuiMouseCursor_::ImGuiMouseCursor_Hand);
 	if (ImGui::IsItemClicked())
@@ -74,13 +77,13 @@ void AboutWindow::SoftwareUsed()
 	if (ImGui::IsItemClicked())
 		App->SendToLink("https://github.com/juj/MathGeoLib");
 
-	ImGui::Text("  Glew 2.1.0");
+	ImGui::Text("  Glew v%s", glewGetString(GLEW_VERSION));
 	if (ImGui::IsItemHovered())
 		ImGui::SetMouseCursor(ImGuiMouseCursor_::ImGuiMouseCursor_Hand);
 	if (ImGui::IsItemClicked())
 		App->SendToLink("http://glew.sourceforge.net/");
 
-	ImGui::Text("  OpenGL vXXX");
+	ImGui::Text("  OpenGL v%s", glGetString(GL_VERSION));
 	if (ImGui::IsItemHovered())
 		ImGui::SetMouseCursor(ImGuiMouseCursor_::ImGuiMouseCursor_Hand);
 	if (ImGui::IsItemClicked())
@@ -94,5 +97,5 @@ void AboutWindow::License()
 	ImGui::NewLine();
 	ImGui::Text("License:");
 	ImGui::NewLine();
-	ImGui::TextWrapped("MIT License \n\nCopyright(c) 2019 ChronoByte \n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions :\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
+	ImGui::TextWrapped("MIT License \n\n\nCopyright(c) 2019 ChronoByte \n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions :\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
 }
