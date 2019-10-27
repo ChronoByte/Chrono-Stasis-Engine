5af6df1cbef692bc77706ff5c082a75c727071ed
The Amazing Sπderman Negro <36167348+CarlosUPC@users.noreply.github.com>
2019-10-27 18:47:37 +0100

    cleaned code

--- Chrono Stasis Engine/Engine/src/Modules/csRendererGUI.cpp
+++ Chrono Stasis Engine/Engine/src/Modules/csRendererGUI.cpp
@@ -419,24 +419,20 @@ void ModuleUI::DockLoader()
 
 	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
 
-	// Seeting Docking to fit the window and preferences
+	
 	ImGuiViewport* viewport = ImGui::GetMainViewport();
 	ImGui::SetNextWindowPos(viewport->Pos);
 	ImGui::SetNextWindowSize(viewport->Size);
 	ImGui::SetNextWindowViewport(viewport->ID);
+	
 	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
 	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
 	window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
 	window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
-	/*if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-		window_flags |= ImGuiWindowFlags_NoBackground;*/
-
 	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 	ImGui::Begin("DockSpace BackGround", (bool*)0, window_flags);
 	ImGui::PopStyleVar(3);
 
-	// DockSpace
 	ImGuiIO& io = ImGui::GetIO();
 	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
 	{
--- Chrono Stasis Engine/Engine/src/Structure/InspectorWindow.cpp
+++ Chrono Stasis Engine/Engine/src/Structure/InspectorWindow.cpp
@@ -40,7 +40,7 @@ void InspectorWindow::Draw()
 			}
 
 			go_selected->DrawInspectorComponents();  
-		   // ShowInspector();
+		   
 		}
 		
 	}
@@ -48,61 +48,3 @@ void InspectorWindow::Draw()
 
 }
 
-void InspectorWindow::ShowInspector()
-{
-	//JUST HOW TO NOT CODE THIS
-	//TODO1 : obtain a gameobject or array of them and loop each one to generate collapsingheaders
-	//TODO2 : create a itemMenu to create on inspector components using GameObject methods
-	//TODO3 : loop a component draw method to put info
-	
-		ImGui::Text("Options");
-		bool enabled = true;
-
-		ImGui::Checkbox("", &enabled);
-		ImGui::SameLine();
-		char name[25];
-		sprintf_s(name, 25, "Cube");
-		ImGui::InputText("", name, 25, ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_EnterReturnsTrue);
-		//Set gameobject name
-
-	
-		if (ImGui::CollapsingHeader("Local Transformation" , ImGuiTreeNodeFlags_DefaultOpen))
-		{
-			
-			float pos[3] = { 0.0f, 0.0f, 0.0f };
-			ImGui::InputFloat3("Position", pos, 2);
-			
-			float rot[3] = { 0.0f, 0.0f, 0.0f };
-			ImGui::InputFloat3("Rotation", rot, 2);
-			
-			float scale[3] = { 1.0f,1.0f, 1.0f };
-			ImGui::InputFloat3("Scale", scale, 2);
-		}
-		if (ImGui::CollapsingHeader("Geometry", ImGuiTreeNodeFlags_DefaultOpen))
-		{
-			//TODO: loop all data of each mesh incrment the value to know the total of each stuff
-			ImGui::Text("Total Vertices:"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%i", 1);
-			ImGui::Text("Total Meshes:"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%i", 1);
-			ImGui::Text("Total Faces:"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%i", 1);
-			ImGui::Text("Total UV:"); ImGui::SameLine();
-			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%i", 1);
-		}
-		if (ImGui::CollapsingHeader("Texture", ImGuiTreeNodeFlags_DefaultOpen))
-		{
-			ImGui::Text("Texture:");
-			ImGui::Image((ImTextureID*)2, ImVec2(150, 150));
-
-			float3 color = float3(20.0f, 0.0f, 40.0f);
-
-			if (ImGui::TreeNodeEx("Color", ImGuiTreeNodeFlags_DefaultOpen))
-			{
-				ImGui::ColorEdit3("", (float*)&color);
-				ImGui::TreePop();
-			}
-			
-		}
-	
-}
