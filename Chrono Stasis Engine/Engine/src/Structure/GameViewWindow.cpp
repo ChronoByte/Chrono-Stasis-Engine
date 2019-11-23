85f83b3a5ea6e4784e29288975374dc3edd16bec
Sebi-Lopez <sebi8818@gmail.com>
2019-11-23 02:50:05 +0100

    Fixed game window aswell
    Should this be this way?

--- Chrono Stasis Engine/Engine/src/Structure/GameViewWindow.cpp
+++ Chrono Stasis Engine/Engine/src/Structure/GameViewWindow.cpp
@@ -15,17 +15,28 @@ GameViewWindow::~GameViewWindow()
 void GameViewWindow::Draw()
 {
 	ImGui::Begin("Game", &active, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
+	//ImGui::SetCursorPos({ -(App->window->width - size.x) / 2,-(App->window->height - size.y) / 2 });
 
 	ImVec2 size = ImGui::GetWindowSize();
-	ImGui::SetCursorPos({ -(App->window->width - size.x) / 2,-(App->window->height - size.y) / 2 });
+
+	width = size.x;
+	height = size.y;
 	
+	if (regionX != ImGui::GetContentRegionAvail().x || regionY != ImGui::GetContentRegionAvail().y)
+	{
+		regionX = ImGui::GetContentRegionAvail().x;
+		regionY = ImGui::GetContentRegionAvail().y;
+
+		App->renderer3D->gameViewport->SetSize(width, height);
+	}
+
 	if (App->scene->GetMainCamera() != nullptr)
 	{
 		if(App->renderer3D->displayZBuffer || App->scene->GetMainCamera()->showZBuffer)
-			ImGui::Image((ImTextureID)App->renderer3D->gameViewport->zBufferTexture, { (float)App->renderer3D->gameViewport->width, (float)App->renderer3D->gameViewport->height }, { 0,1 }, { 1,0 });
+			ImGui::Image((ImTextureID)App->renderer3D->gameViewport->zBufferTexture, { width, height }, { 0,1 }, { 1,0 });
 		
 		else 
-			ImGui::Image((ImTextureID)App->renderer3D->gameViewport->renderTexture, { (float)App->renderer3D->gameViewport->width, (float)App->renderer3D->gameViewport->height }, { 0,1 }, { 1,0 });
+			ImGui::Image((ImTextureID)App->renderer3D->gameViewport->renderTexture, { width, height }, { 0,1 }, { 1,0 });
 	}
 
 	ImGui::End();
